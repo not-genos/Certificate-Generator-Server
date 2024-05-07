12b7c57dcea5fe450033dddd494f50fd3d17035c
Ankush Kashyap <ankumar505@gmail.com>
2024-05-07 22:17:51 +0530

    connection uri updated

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const db = require('mongoose');
 const connectDB = async () => {
     console.log('Connecting to MongoDB');
     try {
-        await db.connect('mongodb://localhost:27017/certificateGenerator');
+        await db.connect(`mongodb+srv://ankush:${process.env.MONGO_PASS}@cluster0.rcrjyha.mongodb.net/certificateGenerator`);
         console.log('Connected to MongoDB');
     } catch (err) {
         console.log('Failed to connect to MongoDB', err);
